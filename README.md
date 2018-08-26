50bb4fe1a68d074aebd90b49c489471dcade2415
Bruno Watt <brunowatt@aviato.local>
2018-08-26 21:10:22 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#### MPA COGNITO SSM IMPORTER
+
+Small script to install cognito ssm values into the stack's environment
+-  this only needs to occur for cognito as that is the only value that is generated on the fly that the front end requires.
---
+++ bin/cognito_ssm_import.js
@@ -0,0 +1,116 @@
+#!/usr/bin/env node
+//we need the region from here so do it first
+const stackOutputPath = `./.build/stackOuts.yaml`;
+const YAML = require( 'yamljs' );
+outputs = YAML.load( stackOutputPath );
+const DEPLOY_REGION = outputs.Region;
+const fs = require('fs');
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: DEPLOY_REGION });
+const ssm = new AWS.SSM();
+
+const COGNITO_IDENTITY_POOL_ID = {
+  ID: 'CognitoIdentityPoolId',
+  SSM: '/mpa/payer/cognito/identitypool/id'
+};
+const COGNITO_USER_POOL_CLIENT_ID = {
+  ID: 'CognitoUserPoolClientId',
+  SSM: '/mpa/payer/cognito/userpool/client/id'
+};
+const COGNITO_USER_POOL_ID = {
+  ID: 'CognitoUserPoolId',
+  SSM: '/mpa/payer/cognito/userpool/id'
+};
+const COGNITO_USER_POOL_ARN = {
+  ID: 'CognitoUserPoolArn',
+  SSM: '/mpa/payer/cognito/userpool/arn'
+};
+const REGION = 'Region';
+//import the output from the serverless process for handling
+
+module.exports.importer = cognitoSsmImporter;
+
+/**
+ * this expect that the identity service has been deployed and the output file is in the correct place.
+ */
+function cognitoSsmImporter() {
+  return new Promise(( resolve, reject ) => {
+    checkCognitoComponents(outputs)
+      .then(result => {
+        Promise.all([
+          ssmUpdate(COGNITO_IDENTITY_POOL_ID),
+          ssmUpdate(COGNITO_USER_POOL_CLIENT_ID),
+          ssmUpdate(COGNITO_USER_POOL_ID),
+          ssmUpdate(COGNITO_USER_POOL_ARN)
+        ])
+          .then(resultArray => {
+            console.log(`ssm updates made`);
+            fs.writeFile(stackOutputPath, '', function(err){
+              if( err ){
+                console.log(`error in file emptyin : `, err)
+              }
+              resolve({result: "OK"});
+            })
+          })
+          .catch(err => {
+            console.log(`error from promise all chain: `, err.message);
+            reject( err )
+          })
+      })
+      .catch(err => {
+        console.log(`error checking  component : `, err.message);
+        reject ( err );
+      });
+  })
+}
+
+function ssmUpdate( record ) {
+  return new Promise(( resolve, reject ) => {
+    let ssmUpdateParams = {
+      Name: record.SSM,
+      Type: 'SecureString',
+      Value: outputs[record.ID],
+      Overwrite: true
+    }
+    ssm.putParameter(ssmUpdateParams).promise()
+      .then( result => {
+        console.log( `success, parameter ${record.ID} version ${ result.Version } update successful` );
+        resolve( result );
+      })
+      .catch( err => {
+        console.log( `error writing ${record.ID} parameter to store` );
+        reject( err );
+      })
+  })
+}
+
+
+
+
+
+/**
+ * Checks the params that the build produced contains those we need
+ * @param outputs
+ * @returns {Promise<any>}
+ */
+function checkCognitoComponents( outputs ) {
+  return new Promise(( resolve, reject ) => {
+    if(!outputs.hasOwnProperty(COGNITO_IDENTITY_POOL_ID.ID)) {
+      reject(new Error('cognito identity pool id not present'))
+    } else if(!outputs.hasOwnProperty( COGNITO_USER_POOL_CLIENT_ID.ID )) {
+      reject(new Error('cognito user pool client id not present'))
+    } else if(!outputs.hasOwnProperty( COGNITO_USER_POOL_ID.ID )) {
+      reject(new Error('cognito user pool id not present'))
+    } else if(!outputs.hasOwnProperty( COGNITO_USER_POOL_ARN.ID )) {
+      reject(new Error('cognito user pool ARN not present'))
+    } else if(!outputs.hasOwnProperty( REGION )) {
+      reject(new Error('region is not present'))
+    } else {
+      console.log(`all requirements present`);
+      resolve({ result: "OK" });
+    }
+  })
+};
+
+cognitoSsmImporter();
